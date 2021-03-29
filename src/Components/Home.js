a7a97eed7fb82ea2a643fc68978c16ec9b310055
manasb <Manas.Bansal@MackenzieHealth.ca>
2021-03-29 15:40:02 -0400

    Made "Modal" component
    Will use later for projects section

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Home.css';
+import './Modal';
+import ModalContainer from './Modal';
 
 function Home() {
     return (
@@ -16,7 +18,7 @@ function Home() {
                     at York University. I love to code and learn new skills. I am interested 
                     in <span>Full Stack Development</span> and <span>Mobile Development</span>
                 </div>
-
+                <ModalContainer />
             </div>
         </div>
     )
