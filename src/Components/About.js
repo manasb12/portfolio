63799888214790bacb8832770acfe67f2c5ca0c6
manasb <Manas.Bansal@MackenzieHealth.ca>
2021-03-31 15:52:18 -0400

    fixed layout

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,31 +1,56 @@
 import React from 'react';
 import SkillBar from './SkillBar.js';
 import './About.css';
-import RenderTab from './RenderTab.js';
+import html from 'programming-languages-logos/src/html/html_48x48.png';
+import css from 'programming-languages-logos/src/css/css_48x48.png';
+import javascript from 'programming-languages-logos/src/javascript/javascript_48x48.png';
+import react from '../img/react_48x48.png';
+import java from 'programming-languages-logos/src/java/java_48x48.png';
+import python from 'programming-languages-logos/src/python/python_48x48.png';
+import sql from '../img/SQL_48x48.png';
 
 function About() {
     return (
         <div id="about" className="aboutContainer">
             <div className="home-desc">
-                <h1 className="section-header">Education</h1>
-                <p>
-                    Throughout my studies at York University, I have taken many practical and theoretical courses including: 
-                    <span> 
-                        <ul>
-                            <li>Advanced Object Oriented Programming</li>
-                            <li>Introduction to Mobile Computing</li>
-                            <li>Introduction to Database Systems</li>
-                            <li>Operating Systems</li>
-                            <li>Theory of Computation</li>
-                            <li>Fundamentals of Data Structures</li>
-                            <li>Software Tools</li>
-                        </ul>
-                    </span>
-                    <p>I have also worked on many school projects, 
-                        assignments and labs
-                    </p>
-                    <p>Currently I have an overall GPA of <span>3.0 / 4.0</span> in my degree</p>
-                </p>
+                <h1 className="section-header">About Me</h1>
+                <hr className="heading-hr"></hr>
+                <div className="skills-container">
+                    <div className="frontEnd-skills">
+                        <div className="frontEnd-skills-list">
+                            <h1>FRONT-END SKILLS</h1>
+                            <ul className="frontEndList">
+                                <li>ReactJS</li>
+                                <li>HTML / CSS</li>
+                                <li>JavaScript</li>
+                                <li>Bootstrap</li>
+                                <li>Responsive Design</li>
+                            </ul>
+                        </div>
+                        <div className="frontEnd-skills-img">
+                            <img src={react}/>
+                            <img src={html}/>
+                            <img src={css}/>
+                            <img src={javascript}/>
+                        </div>
+                    </div>
+                    <hr></hr>
+                    <div className="backEnd-skills">
+                        <div className="backEnd-skills-img">
+                            <img src={java}/>
+                            <img src={python}/>
+                            <img src={sql}/>
+                        </div>
+                        <div className="backEnd-skills-list">
+                            <h1>BACK-END SKILLS</h1>
+                            <ul className="backEndList">
+                                <li>Java</li>
+                                <li>Python</li>
+                                <li>SQL</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
             {/*<div className="home-skills">
                 <h1 className="section-header">Skills</h1>
@@ -40,5 +65,27 @@ function About() {
     )
 }
 
+const Education = () => {
+    return (
+        <p>
+            Throughout my studies at York University, I have taken many practical and theoretical courses including:
+            <span>
+                <ul>
+                    <li>Advanced Object Oriented Programming</li>
+                    <li>Introduction to Mobile Computing</li>
+                    <li>Introduction to Database Systems</li>
+                    <li>Operating Systems</li>
+                    <li>Theory of Computation</li>
+                    <li>Fundamentals of Data Structures</li>
+                    <li>Software Tools</li>
+                </ul>
+            </span>
+            <p>I have also worked on many school projects,
+            assignments and labs
+                    </p>
+            <p>Currently I have an overall GPA of <span>3.0 / 4.0</span> in my degree</p>
+        </p>
+    )
+}
 
 export default About;
--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -3,47 +3,49 @@ import './JobComponent.css';
 import './GlobalStyles.css';
 
 function Experience() {
-    return ( 
+    return (
         <div id="experience" className="main">
             <h1 id="headerTitle">Experience</h1>
-             <hr className="heading-hr"></hr>
-   
-              <div className="experienceContainer">
-                    <SubComponent
-                        src="https://pbs.twimg.com/profile_images/1258154808134922242/sUXk1gdX_400x400.jpg"
-                        jobTitle = "Senior Technical Student"
-                        company = "Toronto Hydro"
-                    />
-                    <ul>
-                        <li>Upcoming Intern at Toronto Hydro in Project Management department starting Summer 2021</li>
-                    </ul>
+            <hr className="heading-hr"></hr>
+            <div className="experience-main-container">
+                <div className="experience-subContainer">
+                    <div className="experienceContainer">
+                        <SubComponent
+                            src="https://pbs.twimg.com/profile_images/1258154808134922242/sUXk1gdX_400x400.jpg"
+                            company="Toronto Hydro"
+                            jobTitle="Senior Technical Student | Summer 2021"
+                        />
+                        <ul>
+                            <li>Upcoming Intern at Toronto Hydro in Project Management department starting Summer 2021</li>
+                        </ul>
+                    </div>
+
+                    <div className="experienceContainer">
+                        <SubComponent
+                            src="https://media-exp1.licdn.com/dms/image/C4E0BAQHt4ygkEQqRng/company-logo_200_200/0/1593551853945?e=1619049600&v=beta&t=G6qibBc88exIO104DLHECUUw8-6c7PvbhwjbQz5GhfY"
+                            company="Cortellucci Vaughan Hospital"
+                            jobTitle="Smart Systems Intern | January 2021 - Present" />
+                        <ul>
+                            <li>Currently working in the ICAT department at Canada's first Smart Hospital</li>
+                            <li>Validated smart devices ensuring they are on, connected and displaying correct information</li>
+                            <li>Performed extensive MRT tests and organized database spreadhseets</li>
+                            <li>Continually collborated with the Application Integration team to validate views expected by clinicians.</li>
+                        </ul>
+                    </div>
                 </div>
-           
-            <hr></hr>
-            <div className="experienceContainer">
-                <SubComponent
-                    src="https://media-exp1.licdn.com/dms/image/C4E0BAQHt4ygkEQqRng/company-logo_200_200/0/1593551853945?e=1619049600&v=beta&t=G6qibBc88exIO104DLHECUUw8-6c7PvbhwjbQz5GhfY"
-                    jobTitle="Smart Systems Intern"
-                    company="Cortellucci Vaughan Hospital"/>
-                <ul>
-                    <li>Currently working in the ICAT department at Canada's first Smart Hospital</li>
-                    <li>Validated smart devices ensuring they are on, connected and displaying correct information</li>
-                    <li>Performed extensive MRT tests and organized database spreadhseets</li>
-                    <li>Continually collborated with the Application Integration team to validate views expected by clinicians.</li>
-                </ul>
-            </div>
-            <hr></hr>
-            <div className="experienceContainer">
-                <SubComponent
-                    src="https://media.glassdoor.com/sqll/141035/york-university-squarelogo.png"
-                    jobTitle="Peer Tutor"
-                    company="Lassonde School of Engineering"/>
-                <ul>
-                    <li>Selected by course instructor to mentor other students due to
-                    {<br></br>}excellent performance in 'Net Centric Introduction to Computing'
-                    </li>
-                    <li>Helped students with flowcharts and coding in HTML, CSS and JavaScript</li>
-                </ul>
+                {/*<hr></hr>
+                <div className="experienceContainer">
+                    <SubComponent
+                        src="https://media.glassdoor.com/sqll/141035/york-university-squarelogo.png"
+                        company="Lassonde School of Engineering"
+                        jobTitle="Peer Tutor" />
+                    <ul>
+                        <li>Selected by course instructor to mentor other students due to
+                        {<br></br>}excellent performance in 'Net Centric Introduction to Computing'
+                        </li>
+                        <li>Helped students with flowcharts and coding in HTML, CSS and JavaScript</li>
+                    </ul>
+                </div>*/}
             </div>
         </div>
     )
@@ -52,9 +54,9 @@ function Experience() {
 function SubComponent(props) {
     return (
         <div>
-            <img src={props.src}/>
-            <h1>{props.jobTitle}</h1>
-            <h3>{props.company}</h3>
+            <img src={props.src} />
+            <h1 className="company">{props.company}</h1>
+            <h3 className="jobTitle">{props.jobTitle}</h3>
         </div>
     )
 }
